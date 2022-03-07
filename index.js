c4a50b8ada743c046a9b333b67e7409035bce7e1
Karan_Kumar_Maurya <karankumarmaurya2002@gmail.com>
2022-03-08 01:43:31 +0530

    setting Page

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import locator from './src/locator';
+import settingPage from './src/settingPage';
 
-AppRegistry.registerComponent(appName, () => locator);
+AppRegistry.registerComponent(appName, () => settingPage);
